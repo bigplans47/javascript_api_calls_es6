ff6ab37aa4ac917a61553ef0734d68e0e98ce45c
Dan Kiss <flowfast47@gmail.com>
2017-10-31 16:46:14 -0700

    add console logs to see inner workings of asynchronous code

--- weather-interface.js
+++ weather-interface.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val('');
-    console.log(5);
+    console.log(5, city);
     let promise = new Promise(function(resolve, reject){
       console.log(7);
       let request = new XMLHttpRequest();
-      console.log(9);
+      console.log(9, request);
       let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=adfda684d78d4876a3e77e8dbfb6c5b9`
-      console.log(11);
+      console.log(11, url);
       request.onload = function() {
-        console.log(13);
+        console.log(13, request);
         if (this.status === 200) {
           console.log(15);
           resolve(request.response);
@@ -20,12 +20,14 @@ $(document).ready(function() {
           reject(Error(request.statusText));
           console.log(21);
         }
+        console.log(23,promise)
         // }
       }
       console.log(25);
       request.open('GET', url, true);
       console.log(27);
       request.send();
+      console.log(29);
     });
 
     promise.then(function(response) {

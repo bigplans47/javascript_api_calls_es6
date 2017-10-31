07349c9af809373bd5e8c1bc60cb6acf8b5bfd99
Dan Kiss <flowfast47@gmail.com>
2017-10-31 16:27:08 -0700

    add promise and then method to make api call and return response to user

---
+++ weather-interface.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val('');
+    console.log(5);
+    let promise = new Promise(function(resolve, reject){
+      console.log(7);
+      let request = new XMLHttpRequest();
+      console.log(9);
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=adfda684d78d4876a3e77e8dbfb6c5b9`
+      console.log(11);
+      request.onload = function() {
+        console.log(13);
+        if (this.status === 200) {
+          console.log(15);
+          resolve(request.response);
+          console.log(17);
+        } else {
+          console.log(19);
+          reject(Error(request.statusText));
+          console.log(21);
+        }
+        // }
+      }
+      console.log(25);
+      request.open('GET', url, true);
+      console.log(27);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.errors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+});
